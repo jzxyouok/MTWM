c9aad8cb74cf81924ca15ca3dab57276a7f4718c
李飞 <lifeiforwork@gmail.com>
2016-06-09 22:54:17 +0800

    项目准备

---
+++ 仿美团外卖/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿美团外卖
+//
+//  Created by 李飞 on 16/6/9.
+//  Copyright © 2016年 FeiGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
